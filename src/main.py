7fe95fbcdebdb780d7a951939f3d7821727783b2
Khady <kheuflo@hotmail.fr>
2023-07-03 23:43:51 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ import time
 
 
 class Main:
+
     def __init__(self):
         self._hub_connection = None
         self.HOST = None  # Setup your host here
@@ -31,27 +32,20 @@ class Main:
             time.sleep(2)
 
     def setSensorHub(self):
-        self._hub_connection = (
-            HubConnectionBuilder()
-            .with_url(f"{self.HOST}/SensorHub?token={self.TOKEN}")
-            .configure_logging(logging.INFO)
-            .with_automatic_reconnect(
-                {
+        self._hub_connection = (HubConnectionBuilder().with_url(
+            f"{self.HOST}/SensorHub?token={self.TOKEN}").configure_logging(
+                logging.INFO).with_automatic_reconnect({
                     "type": "raw",
                     "keep_alive_interval": 10,
                     "reconnect_interval": 5,
                     "max_attempts": 999,
-                }
-            )
-            .build()
-        )
+                }).build())
 
         self._hub_connection.on("ReceiveSensorData", self.onSensorDataReceived)
         self._hub_connection.on_open(lambda: print("||| Connection opened."))
         self._hub_connection.on_close(lambda: print("||| Connection closed."))
         self._hub_connection.on_error(lambda data: print(
-            f"||| An exception was thrown closed: {data.error}")
-        )
+            f"||| An exception was thrown closed: {data.error}"))
 
     def onSensorDataReceived(self, data):
         try:
@@ -69,8 +63,7 @@ class Main:
         elif float(data) <= float(self.T_MIN):
             self.sendActionToHvac(date, "TurnOnHeater", self.TICKETS)
 
-    def sendActionToHvac(
-            self, date, action, nbTick):
+    def sendActionToHvac(self, date, action, nbTick):
         r = requests.get(
             f"{self.HOST}/api/hvac/{self.TOKEN}/{action}/{nbTick}")
         details = json.loads(r.text)
